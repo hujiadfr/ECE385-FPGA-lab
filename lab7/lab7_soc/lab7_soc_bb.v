584b848c605ad0889bf3f5a9bd7e0c9967891fd8
Han <64575983+ArchdukeHans@users.noreply.github.com>
2021-11-09 18:53:52 +0800

    Finish lab7_soc and HDL Generation
    a lot of new sv and v files added in /lab7_soc folder.

---
+++ lab7/lab7_soc/lab7_soc_bb.v
@@ -0,0 +1,30 @@
+
+module lab7_soc (
+	clk_clk,
+	reset_reset_n,
+	lead_wire_export,
+	sdram_clk_clk,
+	sdram_wire_addr,
+	sdram_wire_ba,
+	sdram_wire_cas_n,
+	sdram_wire_cke,
+	sdram_wire_cs_n,
+	sdram_wire_dq,
+	sdram_wire_dqm,
+	sdram_wire_ras_n,
+	sdram_wire_we_n);	
+
+	input		clk_clk;
+	input		reset_reset_n;
+	output	[7:0]	lead_wire_export;
+	output		sdram_clk_clk;
+	output	[12:0]	sdram_wire_addr;
+	output	[1:0]	sdram_wire_ba;
+	output		sdram_wire_cas_n;
+	output		sdram_wire_cke;
+	output		sdram_wire_cs_n;
+	inout	[31:0]	sdram_wire_dq;
+	output	[3:0]	sdram_wire_dqm;
+	output		sdram_wire_ras_n;
+	output		sdram_wire_we_n;
+endmodule
