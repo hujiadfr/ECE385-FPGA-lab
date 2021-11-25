3e33e02032dfa1bd9e7721a6e7a1d809095c52f3
hujiadfr <jiarun126@hotmail.com>
2021-11-25 18:59:40 +0800

    add lab8 sdc

--- lab9/avalon_aes_interface.sv
+++ lab9/avalon_aes_interface.sv
@@ -37,6 +37,68 @@ module avalon_aes_interface (
 	// Exported Conduit
 	output logic [31:0] EXPORT_DATA		// Exported Conduit Signal to LEDs
 );
+logic [15:0][31:0] Reg_unit;
+	logic [3:0][31:0] MSG_DEC;
+	logic Done;
 
+	assign EXPORT_DATA = {Reg_unit[7][31:16],Reg_unit[4][15:0]};
+	always_ff @ (posedge CLK)
+	begin
+		if (RESET)				// if reset is active, clear all registers
+			begin
+				Reg_unit[0]  <= 32'h0;
+				Reg_unit[1]  <= 32'h0;
+				Reg_unit[2]  <= 32'h0;
+				Reg_unit[3]  <= 32'h0;
+				Reg_unit[4]  <= 32'h0;
+				Reg_unit[5]  <= 32'h0;
+				Reg_unit[6]  <= 32'h0;
+				Reg_unit[7]  <= 32'h0;
+				Reg_unit[8]  <= 32'h0;
+				Reg_unit[9]  <= 32'h0;
+				Reg_unit[10] <= 32'h0;
+				Reg_unit[11] <= 32'h0;
+				Reg_unit[12] <= 32'h0;
+				Reg_unit[13] <= 32'h0;
+				Reg_unit[14] <= 32'h0;
+				Reg_unit[15] <= 32'h0;
+			end
+		else if (AVL_WRITE && AVL_CS)
+			// Write
+			begin
+				case (AVL_BYTE_EN)
+					4'b1111: Reg_unit[AVL_ADDR] <= AVL_WRITEDATA;
+					4'b1100:	Reg_unit[AVL_ADDR][31:16] <= AVL_WRITEDATA[31:16];
+					4'b0011: Reg_unit[AVL_ADDR][15:0]  <= AVL_WRITEDATA[15:0];
+					4'b1000: Reg_unit[AVL_ADDR][31:24] <= AVL_WRITEDATA[31:24];
+					4'b0100: Reg_unit[AVL_ADDR][23:16] <= AVL_WRITEDATA[23:16];
+					4'b0010: Reg_unit[AVL_ADDR][15:8]  <= AVL_WRITEDATA[15:8];
+					4'b0001: Reg_unit[AVL_ADDR][7:0] <= AVL_WRITEDATA[7:0];
+					default: Reg_unit[AVL_ADDR] <= 32'b0;
+				endcase
+			end
+		else if (Done && AVL_CS)
+			begin
+				Reg_unit[15][0] = Done;
+				Reg_unit[11:8] = MSG_DEC;
+			end
+	end
+	
+	
+	always_comb
+	begin
+		AVL_READDATA = 32'b0;
+		// Read
+		if (AVL_READ)
+			AVL_READDATA = Reg_unit[AVL_ADDR];
 
+	end
+	
+//	AES aes(.*,
+//			  .AES_START(Reg_unit[14][0]),
+//			  .AES_DONE(Done),
+//			  .AES_KEY(Reg_unit[3:0]),
+//			  .AES_MSG_ENC(Reg_unit[7:4]),
+//			  .AES_MSG_DEC(MSG_DEC));
+			  
 endmodule
---
+++ lab9/output_files/lab9.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE115F29) Path("C:/Users/jiaru/GitHub/ECE385-FPGA-lab/lab9/output_files/") File("lab9.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
