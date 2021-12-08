84f1d0ed71c957c35564a6f5378915507d2ffceb
Han <64575983+ArchdukeHans@users.noreply.github.com>
2021-12-08 23:32:32 +0800

    add some file and update README.md
    1. add some file and
    
    2. update README.md
         add weekly schedule

---
+++ Final-Project/README.md
@@ -0,0 +1,61 @@
+
+# Project Tree
+```
+Final-Project                   
+├─ .gitignore                   
+├─ ball.sv                      
+├─ ballController.sv            
+├─ Color_Mapper.sv              
+├─ game_logic.sv                
+├─ HexDriver.sv                 
+├─ hpi_io_intf.sv               
+├─ lab8_soc.qsys                
+├─ lab8_soc.sopcinfo            
+├─ README.md                    
+├─ ship.sv                      
+├─ software                     
+│  ├─ cy7c67200.h               
+│  ├─ io_handler.c              
+│  ├─ io_handler.h              
+│  ├─ lcp_cmd.h                 
+│  ├─ lcp_data.h                
+│  ├─ main.c                    
+│  ├─ note.md                   
+│  ├─ usb.c                     
+│  └─ usb.h                     
+├─ top_level.sv                 
+├─ vga_clk.ppf                  
+├─ vga_clk.qip                  
+├─ vga_clk.v                    
+├─ VGA_controller.sv            
+└─ workspace.code-workspace     
+
+```
+
+# Weekly Schedule & To Do List
+
+## Programming Tasks
+- Write **top_level.sv**
+  - define some parameters, such as:
+    - constant speed for shell, 
+    - acceleration for ship, 
+    - angle velocity for ship, 
+    - maximum speed for ship.
+- Modify **ball.sv** and ballController.sv in order to control the **shell** 炮弹 movement.
+  - Main difficulty is to add **angle** information in radians, and **speed**.
+  - Rewrite movement algorithm with **angle** and **speed**.
+- Write **ship.sv** to module a ship in order to control the **ship** movement.
+  - Main difficulty is to add **angle** information in radians, and **speed**.
+  - Rewrite movement algorithm with **angle** and **speed**.
+- Write cooperation between shell and ship.sv
+  - initial position of shell is determined by ship position, and angle is determined ship angle.
+    - In the future, we may add independent angle control for shell.
+- Compile the function to read more keys from keyboard.
+
+## Technology and Reading Tasks
+- spirits
+- store
+
+## GitHub Pages
+- https://github.com/tlietz/FPGA_Video_Game_2D_Shooter
+- https://github.com/neiman3/basys3battleship
---
+++ Final-Project/ballController.sv
@@ -0,0 +1,78 @@
+module ballController (input Clk, p2,
+							  input done1, done2, done3, done4,
+							  input[7:0] keycode_0, keycode_1, keycode_2, keycode_3, keycode_4, keycode_5,
+							  //output logic ready1, ready2, ready3, ready4,
+							  output logic [3:0] select,
+							  output logic [3:0] direction
+							  );
+		logic[3:0] which_ball;
+		assign select = which_ball;
+		logic ready;
+		logic[27:0] count;
+		logic up, down, left, right;
+		
+		assign right = keycode_0==8'd79 || keycode_1==8'd79 || keycode_2==8'd79 || keycode_3==8'd79 || keycode_4==8'd79 || keycode_5==8'd79;
+		assign left = keycode_0==8'd80 || keycode_1==8'd80 || keycode_2==8'd80 || keycode_3==8'd80 || keycode_4==8'd80 || keycode_5==8'd80;
+		assign down = keycode_0==8'd81 || keycode_1==8'd81 || keycode_2==8'd81 || keycode_3==8'd81 || keycode_4==8'd81 || keycode_5==8'd81;
+		assign up = keycode_0==8'd82 || keycode_1==8'd82 || keycode_2==8'd82 || keycode_3==8'd82 || keycode_4==8'd82 || keycode_5==8'd82;
+
+		 always_ff @ (posedge Clk)
+		 begin
+		 if(count >= 28'h17D7840) // 50*10^6 / 2 = half of a second wait time before
+			begin
+				count<= count;
+				ready <= 1'b1;
+			end
+		 else
+			begin
+				count <= count + 1;
+				ready <= 1'b0;
+			end
+		 
+		 if(right)
+			begin
+				direction <= 4'b0100;
+			end
+		 else if(left)
+			begin
+				direction <= 4'b1000;
+			end
+		 else if(down)
+			begin
+				direction <= 4'b0010;
+			end
+		 else if(up)
+			begin
+				direction <= 4'b0001;
+			end   
+		 
+		 if( done1 == 1'b1 && ready == 1'b1 && (right || left || down || up))
+                begin
+                    //ready1 = 1'b1;
+						  which_ball <= 4'b0001; 
+						  count <= 28'b0;
+                end  
+				else if ( done2 == 1'b1 && ready == 1'b1 && (right || left || down || up))
+                begin  
+                    //ready2 = 1'b1;
+						  which_ball <= 4'b0010;
+						  count <= 28'b0;
+                end
+            else if ( done3 == 1'b1 && ready == 1'b1 && (right || left || down || up) && ~p2)  
+                begin
+                    //ready3 = 1'b1;
+						  which_ball <= 4'b0100;
+						  count <= 28'b0;
+                end
+				else if ( done4 == 1'b1 && ready == 1'b1 && (right || left || down || up) && ~p2)
+                begin
+                    //ready4 = 1'b1;
+						  which_ball <= 4'b1000;
+						  count <= 28'b0;
+                end
+				else
+					begin
+						which_ball <= 4'b0;
+					end
+		 end		  
+endmodule
